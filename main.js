057dbb630537767d21a50e824ba42500877bc6dd
alexxGmZ <algilbertgomez@gmail.com>
2024-08-21 14:09:57 +0800

    initial files

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true,
+		}
+	});
+
+	win.loadFile('./public/designer.html');
+}
+
+app.whenReady().then(() => {
+	createWindow();
+});
