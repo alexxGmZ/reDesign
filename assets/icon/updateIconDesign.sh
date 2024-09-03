9ed87077709f9b3b26edbd3dd9540ac24b0dca10
alexxGmZ <algilbertgomez@gmail.com>
2024-09-03 18:21:08 +0800

    feat(icon): 32x32 size of icon

--- assets/icon/updateIconDesign.sh
+++ assets/icon/updateIconDesign.sh
@@ -3,7 +3,9 @@
 # reDesign-box
 ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=256:256" ./256/box/256x256.png
 ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=128:128" ./128/box/128x128.png
+ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=32:32" ./32/box/32x32.png
 
 # reDesign-circle
 ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=256:256" ./256/circle/256x256.png
 ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=128:128" ./128/circle/128x128.png
+ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=32:32" ./32/circle/32x32.png
