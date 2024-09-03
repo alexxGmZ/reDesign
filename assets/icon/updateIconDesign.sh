e303ca99c127c941b6dbe5a00fc254568f63cd58
alexxGmZ <algilbertgomez@gmail.com>
2024-09-03 18:15:38 +0800

    refactor: change icon directory and filename.

--- assets/icon/updateIconDesign.sh
+++ assets/icon/updateIconDesign.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # reDesign-box
-ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=256:256" ./256/reDesign-box.png
-ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=128:128" ./128/reDesign-box.png
+ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=256:256" ./256/box/256x256.png
+ffmpeg -y -i ./2000/reDesign-box.png -vf "scale=128:128" ./128/box/128x128.png
 
 # reDesign-circle
-ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=256:256" ./256/reDesign-circle.png
-ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=128:128" ./128/reDesign-circle.png
+ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=256:256" ./256/circle/256x256.png
+ffmpeg -y -i ./2000/reDesign-circle.png -vf "scale=128:128" ./128/circle/128x128.png
