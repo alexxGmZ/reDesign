56c32ddb48af6c66f9d285629b197d9d52847a02
alexxGmZ <algilbertgomez@gmail.com>
2024-09-26 23:32:52 +0800

    refactor: move save-canvas ipcMain channel to a module

--- main.js
+++ main.js
@@ -23,44 +23,7 @@ app.whenReady().then(() => {
 });
 
 ipc.openCanvasFile();
-
-/**
- * Handles the "save-canvas-data" IPC event, which triggers a save dialog and writes
- * canvas data to a JSON file. The file path is chosen by the user, and a response is sent
- * back to the renderer process with the status of the save operation.
- */
-ipcMain.on("save-canvas", async (event, jsonedCanvasData) => {
-   console.log("ipcMain.on('save-canvas-data')");
-
-   const replyChannel = "save-canvas-reply";
-   const { filePath } = await dialog.showSaveDialog({
-      title: "Save Canvas",
-      defaultPath: "untitled.json",
-      filters: [{ name: "JSON", extensions: ["json"] }]
-   });
-
-   if (!filePath) {
-      console.log("Save canvas canceled");
-      return event.reply(replyChannel, {
-         message: "Save canvas canceled",
-      });
-   }
-
-   fs.writeFile(filePath, jsonedCanvasData, (error) => {
-      if (error) {
-         console.log("Save canvas failed", error);
-         return event.reply(replyChannel, {
-            message: "Save canvas failed",
-            error: error
-         });
-      }
-
-      console.log("Canvas saved successfully");
-      return event.reply(replyChannel, {
-         message: "Canvas saved successfully",
-      });
-   });
-});
+ipc.saveCanvas();
 
 /**
  * Handles the "export-canvas-to-png" IPC event, which triggers a save dialog and exports
--- mainJs/ipc.js
+++ mainJs/ipc.js
@@ -42,6 +42,48 @@ function openCanvasFile() {
    });
 }
 
+/**
+ * Handles the "save-canvas-data" IPC event, which triggers a save dialog and writes
+ * canvas data to a JSON file. The file path is chosen by the user, and a response is sent
+ * back to the renderer process with the status of the save operation.
+ *
+ * @returns {Object} The ipcMain message status of saving the canvas or an error.
+ */
+function saveCanvas() {
+   console.log("saveCanvas()");
+   const replyChannel = "save-canvas-reply";
+
+   ipcMain.on("save-canvas", async (event, jsonedCanvasData) => {
+      try {
+         const { filePath } = await dialog.showSaveDialog({
+            title: "Save Canvas",
+            defaultPath: "untitled.json",
+            filters: [{ name: "JSON", extensions: ["json"] }]
+         });
+
+         if (!filePath) {
+            console.log("Save canvas canceled");
+            return event.reply(replyChannel, {
+               message: "Save canvas canceled",
+            });
+         }
+
+         await fs.promises.writeFile(filePath, jsonedCanvasData);
+         console.log("Canvas saved successfully");
+         return event.reply(replyChannel, {
+            message: "Canvas saved successfully",
+         });
+      } catch (error) {
+         console.log("Save canvas failed", error);
+         return event.reply(replyChannel, {
+            message: "Save canvas failed",
+            error: error
+         });
+      }
+   });
+}
+
 module.exports = {
-   openCanvasFile
+   openCanvasFile,
+   saveCanvas
 }
